c6c3c0a7db9faab2cce8ee735cecd16d6724b0d1
tuanba <tuanbui.10103@gmail.com>
2024-11-27 16:41:24 +0700

    feat: update header, footer and web.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,9 +6,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Fonts and Icons -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
+    <!-- CSS Styles -->
     <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
@@ -17,25 +19,27 @@
     <link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css') }}">
     <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
 </head>
 <body>
+<!-- Navigation Bar -->
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
-        <a class="navbar-brand" href="#"><span>Study</span>Lab</a>
+        <a class="navbar-brand" href="{{ url('/') }}"><span>Study</span>Lab</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="oi oi-menu"></span> Menu
         </button>
 
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Course</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Instructor</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                <li class="nav-item active"><a href="{{ url('/') }}" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="{{ url('/about') }}" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="{{ url('/courses') }}" class="nav-link">Courses</a></li>
+                <li class="nav-item"><a href="{{ url('/instructors') }}" class="nav-link">Instructors</a></li>
+                <li class="nav-item"><a href="{{ url('/blog') }}" class="nav-link">Blog</a></li>
+                <li class="nav-item"><a href="{{ url('/contact') }}" class="nav-link">Contact</a></li>
             </ul>
         </div>
     </div>
 </nav>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
+
+// Route dẫn đến trang About
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+// Route dẫn đến trang Courses
+Route::get('/courses', function () {
+    return view('courses');
+})->name('courses');
+
+// Route dẫn đến trang Instructor
+Route::get('/instructors', function () {
+    return view('instructors');
+})->name('instructors');
+
+// Route dẫn đến trang Instructor Details
+Route::get('/instructor-details', function () {
+    return view('instructor-details');
+})->name('instructor.details');
+
+// Route dẫn đến trang Blog
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+// Route dẫn đến trang Login
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
